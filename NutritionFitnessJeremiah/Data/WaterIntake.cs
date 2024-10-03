90def9874b23270e9dc6a6695cdc87a57fcbb17b
player_zero <playerzero745@gmail.com>
2024-10-03 16:01:40 +0100

    add water

---
+++ NutritionFitnessJeremiah/Data/WaterIntake.cs
@@ -0,0 +1,12 @@
+﻿namespace NutritionFitnessJeremiah.Data
+{
+    public class WaterIntake
+    {
+        public int Amount { get; set; }
+    }
+
+    public class WaterTracker
+    {
+        public Dictionary<string, WaterIntake> WaterData { get; set; } = new Dictionary<string, WaterIntake>();
+    }
+}
